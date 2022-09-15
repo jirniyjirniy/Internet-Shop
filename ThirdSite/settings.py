fd59b61495d9ed0b5e8f01ba49ca77b681169429
Nikita <66023572+jirniyjirniy@users.noreply.github.com>
2022-09-15 17:27:50 +0300

    Update settings.py

--- ThirdSite/settings.py
+++ ThirdSite/settings.py
@@ -133,8 +133,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'nik.vardi127@gmail.com'
-EMAIL_HOST_PASSWORD = 'qzxzpvqmrwyinqou'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 LOGOUT_REDIRECT_URL = "/"
